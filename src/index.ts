26572b36354ebfaf5f02bc248aa4516e97b8968c
Riadul Hoque <hoqueriadul@gmail.com>
2020-09-09 09:46:09 -0400

    set up mikroOrm initial

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log("Hello world");
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
+import { Post } from "./entities/Post";
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [Post],
+    dbName: "lireddit",
+    type: "postgresql",
+    debug: !__prod__,
+  });
+};
+
+main();
