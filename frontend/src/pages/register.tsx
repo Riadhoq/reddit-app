62f0108a97d3a00cb72a28f94772eb6a3f0a53a0
Riadul Hoque <hoqueriadul@gmail.com>
2020-09-11 16:19:04 -0400

    add graphql code-gen for register type with

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -3,7 +3,7 @@ import { Formik, Form } from "formik";
 import { Box, Button } from "@chakra-ui/core";
 import { Wrapper } from "../components/Wrapper";
 import { InputField } from "../components/InputField";
-import { useMutation } from "urql";
+// import { useMutation } from "urql";
 import { useRegisterMutation } from "../generated/graphql";
 
 interface registerProps {}
