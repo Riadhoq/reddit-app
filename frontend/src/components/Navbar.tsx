e93df6b62dcfb7a235001bd1857687bb291274af
Riadul Hoque <hoqueriadul@gmail.com>
2020-09-11 17:48:21 -0400

    add navbar, me query

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import { Box, Button, Flex, Link } from "@chakra-ui/core";
+import NextLink from "next/link";
+import { useMeQuery } from "../generated/graphql";
+
+interface NavbarProps {}
+
+export const Navbar: React.FC<NavbarProps> = ({}) => {
+  const [{ data, fetching }] = useMeQuery();
+  let body = null;
+
+  if (fetching) {
+    //data loading
+  } else if (!data.me) {
+    // user not logged in
+    body = (
+      <>
+        <NextLink href="/login">
+          <Link mr={2}>Login</Link>
+        </NextLink>
+        <NextLink href="/register">
+          <Link>Register</Link>
+        </NextLink>
+      </>
+    );
+  } else {
+    // user is logged in
+    body = (
+      <Flex>
+        <Box mr={2}>{data.me.username}</Box>
+        <Button variant="link">Logout</Button>
+      </Flex>
+    );
+  }
+  return (
+    <Flex bg="azure" p={4}>
+      <Box ml={"auto"}>{body}</Box>
+    </Flex>
+  );
+};
--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,12 +1,71 @@
 import { ThemeProvider, CSSReset } from "@chakra-ui/core";
-import { createClient, Provider } from "urql";
+import { Cache, cacheExchange, QueryInput } from "@urql/exchange-graphcache";
+import { createClient, dedupExchange, fetchExchange, Provider } from "urql";
+import {
+  LoginMutation,
+  MeDocument,
+  MeQuery,
+  RegisterMutation,
+} from "../generated/graphql";
+import theme from "../theme";
+
+function typedUpdateQuery<Result, Query>(
+  cache: Cache,
+  qi: QueryInput,
+  result: any,
+  fn: (r: Result, q: Query) => Query
+) {
+  return cache.updateQuery(qi, (data) => fn(result, data as any) as any);
+}
+
 const client = createClient({
   url: "http://localhost:4000/graphql",
   fetchOptions: {
     credentials: "include",
   },
+  exchanges: [
+    dedupExchange,
+    cacheExchange({
+      updates: {
+        Mutation: {
+          login: (_result, args, cache, info) => {
+            typedUpdateQuery<LoginMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.login.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.login.user,
+                  };
+                }
+              }
+            );
+          },
+          register: (_result, args, cache, info) => {
+            typedUpdateQuery<RegisterMutation, MeQuery>(
+              cache,
+              { query: MeDocument },
+              _result,
+              (result, query) => {
+                if (result.register.errors) {
+                  return query;
+                } else {
+                  return {
+                    me: result.register.user,
+                  };
+                }
+              }
+            );
+          },
+        },
+      },
+    }),
+    fetchExchange,
+  ],
 });
-import theme from "../theme";
 
 function MyApp({ Component, pageProps }: any) {
   return (
